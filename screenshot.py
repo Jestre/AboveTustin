9fa814fe040d39e9601f390c2f28939fef849b39
Scott Schulz <swschulz@astrum.com>
2017-01-03 12:18:40 -0500

    Add basic cropping functionality

--- screenshot.py
+++ screenshot.py
@@ -12,6 +12,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from configparser import ConfigParser
 
+
 # Read the configuration file for this application.
 parser = ConfigParser()
 parser.read('config.ini')
@@ -19,6 +20,23 @@ parser.read('config.ini')
 # Assign AboveTustin variables.
 abovetustin_image_width = int(parser.get('abovetustin', 'image_width'))
 abovetustin_image_height = int(parser.get('abovetustin', 'image_height'))
+
+#  Check for Crop settings
+if parser.has_section('crop'):
+    try:
+        from PIL import Image
+        from io import BytesIO
+    except ImportError:
+        print('Image manipulation module "Pillow" not found, cropping disabled')
+        do_crop = False
+    do_crop = parser.getboolean('crop', 'do_crop')
+    if do_crop:
+        crop_width = parser.getint('crop', 'crop_width')
+        crop_height = parser.getint('crop', 'crop_height')
+        crop_overlay = parser.getboolean('crop', 'crop_overlay')
+        print('will crop')
+else:
+    do_crop = False
 
 # Assign dump1090 variables.
 dump1090_map_url = parser.get('dump1090', 'map_url')
@@ -70,7 +88,31 @@ def screenshot(browser, name):
     Takes a screenshot of the browser
     '''
     try:
-        browser.save_screenshot(name)
+        if do_crop:
+            print('cropping activated')
+            #  Grab screenshot rather than saving
+            im = browser.get_screenshot_as_png()
+            im = Image.open(BytesIO(im))
+
+            if crop_overlay:
+                # Grab the rectangle before we chop it off
+                # Make configuraable?
+                highlight = im.crop((860, 120, 1250, 280))
+
+            #  Crop to specifications
+            im = im.crop((0,0, crop_width, crop_height or abovetustin_image_height))
+
+            if crop_overlay:
+                w, h = im.size
+                p, q = highlight.size
+                #  Place image in lower right corner
+                #  with 5 pixel border
+                im.paste(highlight, (w-p - 5, h-q - 5))
+
+            im.save(name)
+            print('crop saved')
+        else:
+            browser.save_screenshot(name)
         print("suucess saving screnshot: %s" % name)
     except:
         print("exception in screenshot()")
